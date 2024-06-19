05d00f2d42f3e6b9bf85c173855fb5c2aa398c85
Dema <80632445+DemaPy@users.noreply.github.com>
2024-06-19 19:17:44 +0200

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function ensureError(value: unknown): ServerResponse<null> {
+export function ensureError(value: unknown) {
   if (value instanceof Error) {
     return {
       data: null,
