e3417802889ce80dc4232759083462c48659052a
DemaPy <benderdema@gmail.com>
2024-08-28 20:43:25 +0200

    templates layout and import changed

---
+++ src/components/index.ts
@@ -0,0 +1,14 @@
+import CreateTemplate from "@/pages/Templates/components/CreateTemplate";
+import PageContainer from "./PageContainer";
+import GridView from "./GridView";
+import TemplateCard from "@/pages/Templates/components/TemplateCard";
+import Heading from "./Heading";
+import PageItemsWrapper from "./PageItemsWrapper";
+export {
+  PageContainer,
+  CreateTemplate,
+  GridView,
+  TemplateCard,
+  Heading,
+  PageItemsWrapper,
+};
--- src/pages/Templates/Templates.tsx
+++ src/pages/Templates/Templates.tsx
@@ -1,23 +1,26 @@
-import PageContainer from "../../components/PageContainer";
 import { PlusCircle } from "lucide-react";
-import CreateTemplate from "./components/CreateTemplate";
-import GridView from "../../components/GridView";
-import TemplateCard from "./components/TemplateCard";
-import Heading from "../../components/Heading";
 import { useFetchTemplates } from "./pages/hooks/useTemplate";
-import ComponentsSkeleton from "../Components/components/ComponentsSkeleton";
 import Error from "../Error/Error";
 import { useState } from "react";
+import ComponentsSkeleton from "../Components/components/ComponentsSkeleton";
+import {
+  CreateTemplate,
+  GridView,
+  Heading,
+  PageContainer,
+  PageItemsWrapper,
+  TemplateCard,
+} from "@/components";
 
 const Templates = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
-  const { data, isError, error, isPending } = useFetchTemplates()
+  const { data, isError, error, isPending } = useFetchTemplates();
 
-  if (isPending) return <ComponentsSkeleton />
+  if (isPending) return <ComponentsSkeleton />;
 
   if (isError) {
-    return <Error error={error} message={error.message} path="/" />
+    return <Error error={error} message={error.message} path="/" />;
   }
 
   return (
@@ -33,7 +36,9 @@ const Templates = () => {
       {isOpen && (
         <CreateTemplate isOpen={isOpen} setClose={() => setIsOpen(false)} />
       )}
-      <GridView items={data.data} component={TemplateCard} />
+      <PageItemsWrapper>
+        <GridView items={data.data} component={TemplateCard} />
+      </PageItemsWrapper>
     </PageContainer>
   );
 };
