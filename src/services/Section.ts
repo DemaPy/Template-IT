8c6100bb1f2da9f990bf8541fca2121276f35a2b
DemaPy <benderdema@gmail.com>
2024-09-19 20:31:47 +0200

    Section placeholders and component placeholders endpoints removed.

--- src/services/Section.ts
+++ src/services/Section.ts
@@ -1,5 +1,5 @@
 import { BASE_URL } from "@/config";
-import { CreatePlaceholders, UpdatePlaceholder } from "./types/Placeholder";
+import { UpdatePlaceholder } from "./types/Placeholder";
 import {
   CreateSection,
   CreateSectionFromComponent,
