0b8649566f3e2a6132bccfc5727af8d600489aaf
DemaPy <benderdema@gmail.com>
2024-10-13 16:15:12 +0200

    DOM Purify FORCE_BODY option added

--- src/pages/Components/components/CreateComponent.tsx
+++ src/pages/Components/components/CreateComponent.tsx
@@ -10,7 +10,10 @@ const CreateComponent = () => {
   const { isPending, mutate } = useCreateComponent();
 
   const handleCreate = (payload: PayloadProps) => {
-    const clean = DOMPurify.sanitize(payload.content);
+    const clean = DOMPurify.sanitize(payload.content, {
+      ADD_TAGS: ["style"],
+      FORCE_BODY: true,
+    });
     const clearBrs = clean.replace(/\n/g, "")
     mutate({
       ...payload,
