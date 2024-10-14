7a017e3c3157e4b4bdb6dd0aad284b34fb030736
DemaPy <benderdema@gmail.com>
2024-10-14 20:13:03 +0200

    fix hero section

--- src/pages/Home/components/Hero.tsx
+++ src/pages/Home/components/Hero.tsx
@@ -7,7 +7,8 @@ const Hero = () => {
   const { isLogged } = useAuth();
 
   return (
-    <section className="flex items-center justify-center h-screen text-center select-none">
+    <section className="flex items-center justify-center min-h-screen text-center select-none relative">
+      <div className="absolute inset-0 backdrop-blur-sm bg-gradient-to-b from-cyan-300/20 to-slate-200/50"/>
       <div className="container max-w-5xl grid place-items-center relative px-4">
         <div className="cursor-pointer hover:scale-105 transition-all rounded-full text-stone-100 text-lg flex font-semibold items-center px-3 py-1 -top-1/3 bg-gradient-to-tr from-slate-900/80 to-blue-950/80">
           enroll now for <span className="text-blue-400 ms-1.5">free</span>
