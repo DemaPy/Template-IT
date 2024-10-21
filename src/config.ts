3c652bb3f316207aad5afb06e907ff0682fd4274
DemaPy <benderdema@gmail.com>
2024-10-21 22:10:50 +0200

    production server url

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,6 @@ type MODES = {
 };
 
 export const MODE: keyof MODES = 1;
-export const PRODUCTION = "https://server-68b46mgi3-demas-projects.vercel.app";
+export const PRODUCTION = "https://server-it-nine.vercel.app";
 export const DEVELOPMENT = "http://localhost:7777";
 export const BASE_URL = MODE ? PRODUCTION : DEVELOPMENT;
